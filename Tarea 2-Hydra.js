1362081d62a856db73188913ab4d1baaf53f35d7
Zai-Aldama <81578712+Zai-Aldama@users.noreply.github.com>
2024-04-10 22:04:59 -0600

    Tarea 2 Hydra- 2203068411
    https://hydra.ojack.xyz/?code=JTBBJTJGJTJGUHJvZ3JhbWFjaSVDMyVCM24lMjB2aXN1YWwlMjAxJTBBJTJGJTJGWmFpcmElMjBMdWNlcm8lMjBBbGRhbWElMjBFc3RlYmFuJTBBdm9yb25vaSgxMSUyQzMuMyUyQzQpLnJvdGF0ZSgoKSUyMCUzRCUzRSUyME1hdGguc2luKHRpbWUpJTIwKiUyMDAuNSkucm90YXRlKDE3JTJDMykuY29sb3IoKCklMjAlM0QlM0UlMjBNYXRoLnNpbih0aW1lJTIwKiUyMDAuMyklMjAqJTIwMC41JTIwJTJCJTIwMC41JTJDJTIwKCklMjAlM0QlM0UlMjBNYXRoLmNvcyh0aW1lJTIwKiUyMDAuNSklMjAqJTIwMC41JTIwJTJCJTIwMC41JTJDJTIwKCklMjAlM0QlM0UlMjBNYXRoLnNpbih0aW1lJTIwKiUyMDAuNyklMjAqJTIwMC41JTIwJTJCJTIwMC41KS5vdXQoKQ%3D%3D

---
+++ Tarea 2-Hydra.js
@@ -0,0 +1,13 @@
+
+
+
+//Programación visual 1
+//Zaira Lucero Aldama Esteban
+voronoi(11,3.3,4).rotate(() => Math.sin(time) * 0.5).rotate(17,3).color(() => Math.sin(time * 0.3) * 0.5 + 0.5, () => Math.cos(time * 0.5) * 0.5 + 0.5, () => Math.sin(time * 0.7) * 0.5 + 0.5).color(1,3,5).out()
+
+
+
+
+
+
+
